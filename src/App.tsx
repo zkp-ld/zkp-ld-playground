68438aa8caaaf0c77d51376ae3d5aae274e5fde3
Dan Yamamoto <dan@iij.ad.jp>
2022-11-15 07:25:36 +0000

    update jsonld-signatures-bbs

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ import { revealTemplate } from "./data/template";
 import Registry from "./components/Registry";
 
 export const CREDENTIAL_HEIGHT = "40vh";
-const VERSION = "v0.1.14";
+const VERSION = "v0.1.15";
 
 const lightTheme = createTheme({
   palette: {
