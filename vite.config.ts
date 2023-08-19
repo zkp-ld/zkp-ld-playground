4c99b34f1b8bbf64b5e88a76300d6f36e7762347
Dan Yamamoto <dan@iij.ad.jp>
2023-08-19 15:00:40 +0000

    update `jsonld-proofs` using patched `json-diff` to avoid the problem with vite see: https://github.com/andreyvit/json-diff/issues/123

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.argv': process.argv, // TODO: temporary solution for json-diff & vite combination (see: https://github.com/andreyvit/json-diff/issues/123)
-    'process.env': process.env, // TODO: temporary solution for json-diff & vite combination (see: https://github.com/andreyvit/json-diff/issues/123)
-  },
   build: {
-    outDir: "dist-v2",
-  }
+    outDir: "dist",
+  },
+  base: './'
 });
