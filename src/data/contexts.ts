533ac558c0817d17b4e3b014f08f656ac46a6f94
Dan Yamamoto <dan@iij.ad.jp>
2023-09-26 07:18:59 +0000

    add zkp-ld context

--- src/data/contexts.ts
+++ src/data/contexts.ts
@@ -3,6 +3,7 @@ import dataIntegrityContext from "./contexts/data-integrity-v1.json";
 import didV1Context from "./contexts/did-v1.json";
 import multikeyV1Context from "./contexts/multikey-v1.json";
 import schemaOrgContext from "./contexts/schemaorg.json";
+import zkpldContext from "./contexts/zkp-ld.json";
 
 const _contexts: [string, any][] = [
   ["https://www.w3.org/2018/credentials/v1", credentialV1Context],
@@ -10,6 +11,7 @@ const _contexts: [string, any][] = [
   ["https://www.w3.org/ns/did/v1", didV1Context],
   ["https://w3id.org/security/multikey/v1", multikeyV1Context],
   ["https://schema.org", schemaOrgContext],
+  ["https://zkp-ld.org/context.jsonld", zkpldContext],
 ];
 
 export const CONTEXTS: [string, string][] = _contexts.map(([k, v]) => [
