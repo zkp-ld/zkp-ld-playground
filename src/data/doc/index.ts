1b767767034eaa1224be93ce23847812afc94df8
Dan Yamamoto <dan@iij.ad.jp>
2023-09-29 09:12:53 +0000

    add an example for selective disclosure with `@set` and `@list`

--- src/data/doc/index.ts
+++ src/data/doc/index.ts
@@ -1,6 +1,7 @@
 import Person1 from "./Person1.json";
 import Person2 from "./Person2.json";
 import Person3 from "./Person3.json";
+import PersonWithChildren from "./PersonWithChildren.json";
 import City from "./City.json";
 import Place from "./Place.json";
 import Vaccine from "./Vaccine.json";
@@ -12,4 +13,5 @@ export const exampleDocs = new Map<string, Object>([
   ["Person3", Person3],
   ["Place", Place],
   ["Vaccine", Vaccine],
+  ["Person with Children", PersonWithChildren],
 ]);
