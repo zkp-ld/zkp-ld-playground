65bb19c4e871baf880812cc783116a081a39c131
Dan Yamamoto <dan@iij.ad.jp>
2024-06-07 16:57:23 +0900

    v2.7.6 (#9)
    ### Added
    
    - Add a new feature to allow users to select the playground version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,28 @@
 
 set -e
 
+if [ -z "$1" ]
+then
+    echo "Error: No version provided"
+    exit 1
+fi
+
+semver_regex="^[0-9]+\.[0-9]+\.[0-9]+$"
+
+if [[ ! $1 =~ $semver_regex ]]
+then
+    echo "Error: Version does not match the semver format (X.Y.Z)"
+    exit 1
+fi
+
 git checkout gh-pages 
 
-rm -rf asset
+rm -rf assets
 cp -r dist/* .
+cp -r dist/ v$1/
 
 git add .
-git commit -m "Update"
+git commit -m "Update to v$1"
 git push origin gh-pages
 
 git checkout main
